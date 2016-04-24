00b816fd4b2ebd588a5f789053547b474b924692
Esteve Julià <ejulimelis@ebay.com>
2016-04-24 22:24:23 +0200

    Green

--- src/main/java/com/dodevjutsu/kata/marsrover/Rover.java
+++ src/main/java/com/dodevjutsu/kata/marsrover/Rover.java
@@ -17,7 +17,11 @@ public class Rover {
         } else if (sequence.equals("r")) {
             this.direction = direction.rotateRight();
         } else if (sequence.equals("f")) {
-            this.y += 1;
+            if (this.direction.equals(Direction.facing("N"))) {
+                this.y += 1;
+            } else if (this.direction.equals(Direction.facing("W"))) {
+                this.x -= 1;
+            }
         }
     }
 
