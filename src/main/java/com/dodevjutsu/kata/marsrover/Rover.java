92a5fc77886f7a1018729865a55608da1db8e0e0
Esteve Julià <ejulimelis@ebay.com>
2016-04-25 00:08:33 +0200

    Refactor: remove now unused rover point and direction

--- src/main/java/com/dodevjutsu/kata/marsrover/Rover.java
+++ src/main/java/com/dodevjutsu/kata/marsrover/Rover.java
@@ -2,13 +2,9 @@ package com.dodevjutsu.kata.marsrover;
 
 public class Rover {
     private Vector vector;
-    private Direction direction;
-    private Point point;
 
     public Rover(int x, int y, String direction) {
-        this.point = new Point(x, y);
-        this.direction = Direction.facing(direction);
-        this.vector = new Vector(this.point, this.direction);
+        this.vector = new Vector(new Point(x, y), Direction.facing(direction));
     }
 
     public void receive(String sequence) {
@@ -25,16 +21,12 @@ public class Rover {
     private void execute(String command) {
         if (command.equals("l")) {
             this.vector = vector.rotateLeft();
-            this.direction = direction.rotateLeft();
         } else if (command.equals("r")) {
             this.vector = vector.rotateRight();
-            this.direction = direction.rotateRight();
         } else if (command.equals("f")) {
             this.vector = vector.moveForward();
-            this.point = this.direction.moveForward(this.point);
         } else if (command.equals("b")) {
             this.vector = vector.moveBackwards();
-            this.point = this.direction.moveBackwards(this.point);
         }
     }
 
